59c994de71e1afba767b3f34c1fc45eb364fe66d
Kevin Xue <ivexnuke@gmail.com>
2017-06-24 08:43:23 -0400

    Imported resources for basic prototype

---
+++ Unshackled/GestureController.swift
@@ -0,0 +1,13 @@
+//
+//  Gestures.swift
+//  Unshackled
+//
+//  Created by Kevin Xue on 2017-06-20.
+//  Copyright © 2017 Kevin Xue. All rights reserved.
+//
+
+import SpriteKit
+
+class GestureController {
+    
+}
---
+++ Unshackled/Utility.swift
@@ -0,0 +1,13 @@
+//
+//  Extensions.swift
+//  Unshackled
+//
+//  Created by Kevin Xue on 2017-06-22.
+//  Copyright © 2017 Kevin Xue. All rights reserved.
+//
+
+import SpriteKit
+
+protocol EventListenerNode{
+    func didMoveToScene()
+}
